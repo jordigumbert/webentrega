cf1f39e67fa090b0e1c63dba02c7006b5d6983d6
LT2BS27482 <sandracalistote@hotmail.com>
2026-03-13 17:26:43 +0100

    arregalt el toggle de login/regster

--- src/login.js
+++ src/login.js
@@ -1,19 +1,19 @@
 const no_account = document.querySelectorAll(".signup")
 const si_account = document.querySelectorAll(".signin")
-//console.log(no_account);
+console.log(no_account);
 // retorna un array amb tots els botons marcats amb clase signup 
 
-const btn_sinup=document.querySelector("#btn_singup")
+const btn_sinup=document.querySelector("#btn_signup")
 //console.log(btn_sinup)
 
 btn_sinup.addEventListener("click", (btn) => {
     btn.preventDefault();
-    console.log("click")
-    
-    document.querySelector("#btn_singup").classList.add("oculto")
+    //console.log("click")
+
+    //document.querySelector("#btn_signup").classList.add("oculto")
 
     no_account.forEach((elem) => {
-        console.log(elem)
+        //console.log(elem)
         elem.classList.toggle("oculto")
     } )
 
