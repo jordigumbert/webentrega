58f4caa4e689927ae66cf2b54bf5a82b2d40d983
LT2BS27482 <sandracalistote@hotmail.com>
2026-03-13 16:47:45 +0100

    creat script oculta/mostra per login/registre

--- src/login.js
+++ src/login.js
@@ -1,6 +1,6 @@
 const no_account = document.querySelectorAll(".signup")
 const si_account = document.querySelectorAll(".signin")
- console.log(no_account);
+//console.log(no_account);
 // retorna un array amb tots els botons marcats amb clase signup 
 
 const btn_sinup=document.querySelector("#btn_singup")
@@ -9,7 +9,9 @@ const btn_sinup=document.querySelector("#btn_singup")
 btn_sinup.addEventListener("click", (btn) => {
     btn.preventDefault();
     console.log("click")
+    
     document.querySelector("#btn_singup").classList.add("oculto")
+
     no_account.forEach((elem) => {
         console.log(elem)
         elem.classList.toggle("oculto")
