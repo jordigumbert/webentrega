c94bf5383c8fa324c2a84b75f2cd615f6a622178
LT2BS27482 <sandracalistote@hotmail.com>
2026-03-13 15:41:45 +0100

    refactor pagina a pagina i separat arxius js,css,html

---
+++ src/login.js
@@ -0,0 +1,8 @@
+const no_account = document.querySelectorAll(".signup")
+const si_account = document.querySelectorAll(".signin")
+// console.log(no_account);
+// retorna un array amb tots els botons marcats amb clase signup 
+
+const btn_sinup=document.querySelector("#btn_singup")
+ console.log(btn_sinup)
+
