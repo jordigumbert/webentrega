c48aa2bd7cce94772539493eaf0783f561756e8d
LT2BS27482 <sandracalistote@hotmail.com>
2026-03-16 16:53:36 +0100

    adreçant  validacio errors

--- src/login.js
+++ src/login.js
@@ -1,7 +1,7 @@
 import * as z from 'https://cdn.jsdelivr.net/npm/zod@4.3.6/+esm'
 
 
-const validation = z.object({
+const validar_reg = z.object({
 
     nom: z.string()
         .min(5, "El nom ha de ser de mínim 5 caràcters ")
@@ -14,11 +14,29 @@ const validation = z.object({
         .regex(/[!@#$%^&*(),.?":{}|<>]/, "La contrasenya ha de contenir almenys un caràcter especial")
     ,
     email: z.string()
-        .email(`${Email_} no té format de correu`)
+        .email(`no tens un correu  amb el format esperat` )
     ,
-    confirm: z.string()
+    confirm_: z.string()
+        .min(8, "La contrasenya ha de tenir almenys 8 caràcters")
+        .max(20, "La password no pot ser de més de 20 caràcters"),
+
+    TiCondicions: z.boolean()
+        .refine(check => check === true, {
+            message: "Has d'acceptar les condicions",
+        })
 
 });
+
+
+const validar_login = z.object({ 
+nom: z.string()
+        .min(5, "El nom ha de ser de mínim 5 caràcters ")
+        .max(20, " El nom d'usuari no pot ser de més de 20 caràcters")
+    ,
+    password: z.string()
+        .min(8, "La contrasenya ha de tenir almenys 8 caràcters")
+        .max(20, "La password no pot ser de més de 20 caràcters")
+    })
 
 
 
@@ -32,11 +50,11 @@ const btn_crear_cuenta = document.querySelector("#btn_crear_cuenta")
 const btn_sinup = document.querySelector("#btn_signup")
 //console.log(btn_sinup)
 
-const nom_user_ = document.querySelector("#user_name")
-const password_ = document.querySelector("#password")
-const confirm_ = document.querySelector("#confirm")
-const Email_ = document.querySelector("#email")
-const TandC = document.querySelector("#checkbox_input")
+let nom_user_ = document.querySelector("#user_name")
+let password_ = document.querySelector("#password")
+let confirm_ = document.querySelector("#confirm")
+let Email_ = document.querySelector("#email")
+let TandC = document.querySelector("#checkbox_input")
 
 
 
@@ -55,10 +73,18 @@ btn_sinup.addEventListener("click", (btn) => {
 btn_login.addEventListener("click", (cosa) => {
     cosa.preventDefault();
     // const DATA= querySelector(document.FormData)
-    console.log("has intentat fer login amb user: ", nom_user_.value)
-    console.log("has intentat fer login amb pass: ", password_.value)
-    nom_user_ = nom_user_.value
-    password_ = password_.value
+    // console.log("has intentat fer login amb user: ", nom_user_.value)
+    // console.log("has intentat fer login amb pass: ", password_.value)
+    //nom_user_ = nom_user_.value
+    //password_ = password_.value
+
+    const entrat = {
+        nom: nom_user_.value
+        ,
+        password: password_.value,
+        
+    }
+    console.log(validar.safeParse(entrat))
 
 })
 
@@ -71,12 +97,28 @@ btn_crear_cuenta.addEventListener("click", (cosa) => {
         console.log("has intentat fer login amb pass_confirm: ", confirm_.value)
         console.log("has intentat fer login amb Email: ", Email_.value)
         console.log("Tens el checkbox en : ", TandC.checked)
-    */
     nom_user_ = nom_user_.value
     password_ = password_.value
     Email_ = Email_.value
     confirm_ = confirm_.value
-    TandC = TandC.checked
-    
-    /* creem la var amb el nom del que conté directament, aixi ens estalviem el .value cada cop. */
+    TandC = TandC.checked 
+    */
+    const entrat = {
+        nom: nom_user_.value,
+
+        password: password_.value,
+        confirm_: confirm_.value,
+
+        email: Email_.value,
+
+        TiCondicions: TandC.checked,
+    }
+
+    const errObj = validar_reg.safeParse(entrat) 
+    //console.log(  errObj  )
+    //console.log("\n\n-------------\n\n", errObj.error.issues.message  ) 
+    errObj.error.issues.forEach( (ERR) => {
+        console.log( "-----\n", ERR, "-----\n")
+
+    })
 })
